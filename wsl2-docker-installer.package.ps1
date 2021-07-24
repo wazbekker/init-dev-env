89cff68f2f7c37784ddac180c518b2bc6f3f0069
Warick Bekker <wazbekker@gmail.com>
2021-07-24 08:42:04 +0200

    uncomment to install wsl2

--- wsl2-docker-installer.package.ps1
+++ wsl2-docker-installer.package.ps1
@@ -1,8 +1,10 @@
 # Enable-WindowsOptionalFeature -online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+# dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 # Enable-WindowsOptionalFeature -online -FeatureName VirtualMachinePlatform -NoRestart
+# dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
 choco install Microsoft-Windows-Subsystem-Linux --source windowsFeatures
 choco install VirtualMachinePlatform --source windowsFeatures
-# choco upgrade wsl2 --params="/Version:2 /Retry:true" --yes
-# choco upgrade wsl-ubuntu-2004 --yes
+choco upgrade wsl2 --params="/Version:2 /Retry:true" --yes
+choco upgrade wsl-ubuntu-2004 --yes
 # choco upgrade docker-desktop --yes
