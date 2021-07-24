45e56ec0e238c0333d05b5f99de5b3c3927cdb90
Warick Bekker <wazbekker@gmail.com>
2021-07-24 09:09:48 +0200

    downgrade to ubuntu 1804

--- wsl2-docker-installer.package.ps1
+++ wsl2-docker-installer.package.ps1
@@ -6,5 +6,5 @@
 choco install Microsoft-Windows-Subsystem-Linux --source windowsFeatures
 choco install VirtualMachinePlatform --source windowsFeatures
 choco upgrade wsl2 --params="/Version:2 /Retry:true" --yes
-choco upgrade wsl-ubuntu-2004 --yes
+choco upgrade wsl-ubuntu-1804 --yes
 # choco upgrade docker-desktop --yes
