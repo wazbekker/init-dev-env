7e339ae2d4c91a4e6c82432136e6a1c9b5c98f67
Warick Bekker <wazbekker@gmail.com>
2021-07-25 14:11:19 +0200

    test

--- initialize-dev-environment.ps1
+++ initialize-dev-environment.ps1
@@ -21,7 +21,7 @@ $installPackages = @(
     # "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-oracleinstantclient.ps1",
     # "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-plsqldeveloper.ps1",
     # "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-nodejs.ps1",
-    "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-wsl2.ps1",
+    "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-wsl2.ps1"
     # "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-dockerdesktop.ps1",
     # "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-misc.ps1"
 )
