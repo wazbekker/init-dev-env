1f39b1691b55f3d8a84a875e2547cf986c7d9e8f
Warick Bekker <wazbekker@gmail.com>
2021-07-25 18:57:30 +0200

    test

--- create-localoracledb.ps1
+++ create-localoracledb.ps1
@@ -1,8 +1,8 @@
 # pull docker image from docker hub for local oracle db
-# docker pull wazbekker/oracledatabase:12.2.0.1-se2-extended
+docker pull wazbekker/oracledatabase:12.2.0.1-se2-extended
 
 # start container using oracle docker image
-# docker run --name ora122 -p 1521:1521 -p 5500:5500 -v oradata:/opt/oracle/oradata wazbekker/oracledatabase:12.2.0.1-se2-extended
+docker run --name ora122 -p 1521:1521 -p 5500:5500 -v oradata:/opt/oracle/oradata wazbekker/oracledatabase:12.2.0.1-se2-extended
 
 # create mtp user with correct permissions
 # sqlplus
