4cc5559219ce6f251f484167b4cd56550de14769
Warick Bekker <wazbekker@gmail.com>
2021-07-25 06:56:07 +0100

    test

---
+++ wsl2-installer.package.ps1
@@ -0,0 +1,11 @@
+# Enable-WindowsOptionalFeature -online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+# dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+# Enable-WindowsOptionalFeature -online -FeatureName VirtualMachinePlatform -NoRestart
+# dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+choco install Microsoft-Windows-Subsystem-Linux --source windowsFeatures
+choco install VirtualMachinePlatform --source windowsFeatures
+choco upgrade wsl2 --params="/Version:2 /Retry:true" --yes
+choco upgrade wsl-ubuntu-2004 --yes
+
+ubuntu2004
