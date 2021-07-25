0b3caa34f3ef7718b3af8fc2a6f0884e0e198745
Warick Bekker <wazbekker@gmail.com>
2021-07-25 08:08:56 +0100

    refactor

--- initialize-dev-environment.ps1
+++ initialize-dev-environment.ps1
@@ -4,18 +4,26 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 $cred=Get-Credential -credential bekkerw
 #$cred=Get-Credential -credential domain\username
 
-$uninstallPackages = @(
-    "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-wsl2.ps1",
-    "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-nodejs.ps1",
-    "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-visualstudio2019.ps1",
-    "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-vscode.ps1"
-    "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-wsl2.ps1"
+# $uninstallPackages = @(
+#     "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-wsl2.ps1",
+#     "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-nodejs.ps1",
+#     "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-visualstudio2019.ps1",
+#     "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-vscode.ps1"
+#     "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-git.ps1"
+# )
+
+# Install-BoxstarterPackage -PackageName $uninstallPackages -Credential $cred
+
+$installPackages = @(
+    "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-git.ps1",
+    "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-vscode.ps1",
+    # "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-visualstudio2019.ps1",
+    # "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-oracleinstantclient.ps1",
+    # "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-plsqldeveloper.ps1",
+    # "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-nodejs.ps1",
+    "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-wsl2.ps1",
+    "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-dockerdesktop.ps1",
+    "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-misc.ps1"
 )
 
-Install-BoxstarterPackage -PackageName $uninstallPackages -Credential $cred
-
-$packages = @(
-    "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-wsl2.ps1"
-)
-
-Install-BoxstarterPackage -PackageName $packages -Credential $cred
+Install-BoxstarterPackage -PackageName $installPackages -Credential $cred
