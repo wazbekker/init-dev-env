cd18c7357227d3eabc69600c41f9f4a156831417
Warick <wazbekker@gmail.com>
2021-07-30 16:45:11 +0200

    Update initialize-dev-environment.ps1

--- initialize-dev-environment.ps1
+++ initialize-dev-environment.ps1
@@ -1,19 +1,12 @@
 # install BoxStarter which will install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://boxstarter.org/bootstrapper.ps1')); Get-Boxstarter -Force
 
+# Modify user below to your own user, this must match your windows user 
+# so that the script can automatically reboot and continue when it needs to
+# Example: $cred=Get-Credential -credential domain\username
 $cred=Get-Credential -credential bekkerw
-#$cred=Get-Credential -credential domain\username
 
-# $uninstallPackages = @(
-#     "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-wsl2.ps1",
-#     "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-nodejs.ps1",
-#     "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-visualstudio2019.ps1",
-#     "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-vscode.ps1"
-#     "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-git.ps1"
-# )
-
-# Install-BoxstarterPackage -PackageName $uninstallPackages -Credential $cred
-
+# comment out or uncomment urls below to install software needed
 $installPackages = @(
     # "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-git.ps1",
     # "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/install-vscode.ps1",
@@ -31,4 +24,13 @@ $installPackages = @(
 
 Install-BoxstarterPackage -PackageName $installPackages -Credential $cred
 
-# ubuntu2004
+# TODO: Still need to test uninstalling
+# $uninstallPackages = @(
+#     "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-wsl2.ps1",
+#     "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-nodejs.ps1",
+#     "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-visualstudio2019.ps1",
+#     "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-vscode.ps1"
+#     "https://raw.githubusercontent.com/wazbekker/init-dev-env/master/uninstall-git.ps1"
+# )
+
+# Install-BoxstarterPackage -PackageName $uninstallPackages -Credential $cred
